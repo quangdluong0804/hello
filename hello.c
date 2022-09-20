3bf81ad526c0c23e6bc7b0dfa68d8f5406bfc299
quangdluong0804 <quangdluong0804@huskers.unl.edu>
2022-09-20 09:06:46 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Charles Luong
+ * Date: 2022/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello Charles!\n");
+
+  return 0;
+}
